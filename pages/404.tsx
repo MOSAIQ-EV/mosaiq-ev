6cecc9365fc1615aef7d8e50aafb1f78c3792885
Simon Ruisinger <s.ruisinger@googmail.com>
2021-02-09 23:26:02 +0100

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+import PageHeadline from "../components/PageHeadline";
+export default function FourOhFour() {
+  return <PageHeadline headline="Da ist wohl etwas schief gelaufen..." />;
+}
